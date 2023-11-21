9dd8df6ae7a0693e532f7a1fc213db1cbba57838
Madina <madinanuriddinova949gmail.com>
2023-11-21 15:06:19 +0600

    DZ

---
+++ app.js
@@ -0,0 +1,67 @@
+//1
+
+var a = prompt('Введите месяц')
+switch (a) {
+    case 'Январь':
+        console.log('Зима')
+        break
+    case 'Февраль':
+        console.log('Зима')
+        break
+    case 'Март':
+        console.log('Весна')
+        break
+    case 'Апрель':
+        console.log('Весна')
+        break
+    case 'Май':
+        console.log('Весна')
+        break
+    case 'Июнь':
+        console.log('Лето')
+        break
+    case 'Июль':
+        console.log('Лето')
+        break
+    case 'Август':
+        console.log('Лето')
+        break
+    case 'Сентябрь':
+        console.log('Осень')
+        break
+    case 'Октябрь':
+        console.log('Осень')
+        break
+    case 'Ноябрь':
+        console.log('Осень')
+        break
+    case 'Декабрь':
+        console.log('Зима')
+        break
+}
+
+
+
+//2
+
+const array = [[[[[[[[[[[2]]]]]]]]]]]
+// //индекс
+console.log(array[0][0][0][0][0][0][0][0][0][0][0])
+
+
+//3
+
+switch (a) {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        result = num1 / num2;
+        break;
+}
